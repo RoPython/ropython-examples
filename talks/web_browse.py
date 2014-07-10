9a4f83c8b7e999d598a5c196097635ac0a9cbf1c
Alexandru Coman <mail@alexcoman.com>
2014-07-10 18:28:33 +0300

    Added web browsing and URL parsing example.

---
+++ talks/web_browse.py
@@ -0,0 +1,19 @@
+""" Parse the CNN rss feed and open the most recent
+10 links in the browser.
+"""
+ 
+import webbrowser
+import requests
+
+from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
+
+def get_urls(max_urls=None):
+    handle = requests.get("http://rss.cnn.com/rss/edition_technology.rss")
+    soup = BeautifulSoup(handle.content)
+    return [item.findAll("guid")[0].text
+            for item in soup.find_all('item')][:max_urls]
+
+executor = ThreadPoolExecutor(max_workers=3)
+for url in get_urls(max_urls=10):
+    executor.submit(webbrowser.open, url)
